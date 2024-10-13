e46db7c9e271b34e35d599701d94c27c60788158
daisuke19891023 <okadaih11023.gmail.com>
2024-10-14 02:14:25 +0900

    create lint env

---
+++ noxfile.py
@@ -0,0 +1,40 @@
+import platform
+import sys
+from pathlib import Path
+
+import nox
+from nox.sessions import Session
+
+
+def constraints(session: Session) -> Path:
+    # Automatically create constraints file name
+    filename = f"python{session.python}-{sys.platform}-{platform.machine()}.txt"
+    return Path("constraints", filename)
+
+
+@nox.session(python=["3.12"], venv_backend="uv")
+def lock(session: Session) -> None:
+    filename = constraints(session)
+    filename.parent.mkdir(exist_ok=True)
+    session.run(
+        "uv",
+        "pip",
+        "compile",
+        "pyproject.toml",
+        "--upgrade",
+        "--quiet",
+        "--all-extras",
+        f"--output-file={filename}",
+    )
+
+
+@nox.session(python=["3.12"])
+def lint(session: Session) -> None:
+    session.install("-c", constraints(session).as_posix(), "ruff")
+    session.run("ruff", "check")
+
+
+@nox.session(python=["3.12"])
+def typing(session: Session) -> None:
+    session.install("-c", constraints(session).as_posix(), ".[typing]")
+    session.run("mypy")
